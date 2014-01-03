92162fcd3d217c080c47a00cce65303b8095ba47
Anders <anders@favoritemedium.com>
2014-01-03 14:16:30 +0900

    Functional test now checks for different urls for different users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -39,6 +39,8 @@ class NewVisitorTest(LiveServerTestCase):
         # When he hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list.
         inputbox.send_keys(Keys.ENTER)
+        arirang_list_url = self.browser.current_url
+        self.assertRegex(arirang_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting him to add another item.
@@ -51,11 +53,33 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        # Arirang wonders whether the site will remember his list.  Then he
-        # sees that the site has generated a unique URL for him -- there is
-        # some explanatory text to that effect.
+        # Now a new user, Seurirang, comes along to the site.
+        self.browser.quit()
+        self.browser.webdriver.Firefox()
+
+        # Seurirang visits the home page.  There is no sign of Arirang's list.
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Seurirang starts a new list by entering a new item.  Hi is less
+        # interesting than Arirang.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Seurirang gets his own unique URL.
+        seurirang_list_url = self.browser.current_url
+        self.assertRegex(seurirang_list_url, '/lists/.+')
+        self.assertNotEqual(seurirang_list_url, arirang_list_url)
+
+        # Again, there is no trace of Arirang's list.
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+
+
+        # Satisfied, Arirang goes back to sleep.
         self.fail('Finish the test!')
-
-        # He visits that URL -- his to-do list is still there.
-
-        # Satisfied, he goes back to sleep.
