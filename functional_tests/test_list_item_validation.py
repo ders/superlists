1d7e61de96c8d78f7c3b166b7c3ac41ac78c9a66
Anders <anders@favoritemedium.com>
2014-01-14 17:18:55 +0900

    Cannot add empty list items

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -22,7 +22,7 @@ class ItemValidationTest(FunctionalTest):
 
         # He receives a similar warning on the list page.
         self.check_for_row_in_list_table('1: Buy milk')
-        error = self.browser.find_element_by_css_selector('.has_error')
+        error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # And he can correct it by filling some list text in
